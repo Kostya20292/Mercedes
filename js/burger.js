e20ad39b266ad30db8f6a076e76f43ea5f7e81b8
Kostya20292 <78802135+Kostya20292@users.noreply.github.com>
2021-09-23 13:51:17 +0300

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,17 @@
+const burger = document.querySelector(".humburger-menu");
+const menuElem = document.querySelector(".menu");
+const menuListLink = document.querySelectorAll(".menu-list__link");
+
+const toggleMenu = () => {
+  menuElem.classList.toggle("menu-active");
+  burger.classList.toggle("humburger-menu-active");
+};
+
+burger.addEventListener("click", () => toggleMenu());
+
+menuListLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuElem.classList.remove("menu-active");
+    burger.classList.remove("humburger-menu-active");
+  });
+});
